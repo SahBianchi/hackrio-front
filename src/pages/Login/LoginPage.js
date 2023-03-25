300974c5c047585f53165c42d67a5ced60e7dbf8
SahBianchi <samantha.sbianchi@gmail.com>
2023-03-25 16:01:25 -0300

    :lipstick: feat:Estilizando a pagina de login

--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -1,26 +1,40 @@
 import React from 'react'
 import Button from '../../componentes/Button'
+import styles from '../Login/style.module.css'
+import logoDonko from '../../assets/logoDonko.png'
+import linha from '../../assets/lineLogin.png'
 
 export const LoginPage = () => {
   return (
     <>
-        <div>
-            <h1>Aqui vai ficar a logo</h1>
-            <h2>Cultura na palma da sua mão</h2>
+    <main>
+        <div className={styles.containerHeader}>
+            <img src={logoDonko} alt='Image Logo'className={styles.logoLogin}/>
+            <h2 className={styles.txtSlogan}>Cultura na palma da sua mão</h2>
         </div>
 
         <div>
-            <input type="email" id="email" name="email" placeholder="E-mail" class="inputPadrao" />
+            <form className={styles.containerForm}>
+                <input type="email" id="email" name="email" placeholder="E-mail" class="inputPadrao" className={styles.inputLogin}/>
 
-            <input type="password" id="password" name="password" placeholder="Senha" />
-        </div>
+                <input type="password" id="password" name="password" placeholder="Senha" className={styles.inputLogin}/>
 
-        <div>
-            <Button type="primary" onClick={()=>{
-                console.log("fui clicado")
-            }}>Continuar</Button>
+                <div className={styles.buttonLogin}>
+                    <Button type="primary" onClick={()=>{
+                    console.log("fui clicado")
+                    }}>Continuar</Button>
+                </div>
+                
+            </form>
         </div>
         
+        <div>
+            <hr className={styles.linhaLogin} />
+            <Button type="secondary" onClick={()=>{
+                    console.log("fui clicado")
+                }}>Crie uma conta!</Button>
+        </div>
+    </main>
     </>
   )
 }
