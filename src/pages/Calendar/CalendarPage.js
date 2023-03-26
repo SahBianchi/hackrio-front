f8ab80b6d6ba43bb3aa5e94efed2e4ffcc17bc80
SahBianchi <samantha.sbianchi@gmail.com>
2023-03-26 18:14:32 -0300

    :recycle: refactor: Atualizações

--- src/pages/Calendar/CalendarPage.js
+++ src/pages/Calendar/CalendarPage.js
@@ -42,63 +42,38 @@ getEvents()
         <div className={styles.containerCalendar}>
             <img src={calendarImg} alt='Image Logo' className={styles.calendarImg} />
 
-            <LineDividing/>
-
-
             {/* TESTE INFOHOUR VERDE */}
             {events.length>0 &&
         events.map((event)=>{
           return(
-            <div key={event.id}className={styles.containerEvento}>
-                <p>Rua {event.adress.rua}, {event.adress.numero} - {event.adress.bairro} - {event.adress.cep}</p>
-                
-                <div className={styles.containerDetails}>
-                  <div className={styles.containerInfos}>
-                    <div className={styles.title}>
-                      <p>{event.name}</p>
-                      <p>Local: {event.empresa.name}</p>
+            <>
+              <LineDividing />
+              <div key={event.id}className={styles.containerEvento}>
+                  <p>Rua {event.adress.rua}, {event.adress.numero} - {event.adress.bairro} - {event.adress.cep}</p>
+                  
+                  <div className={styles.containerDetails}>
+                    <div className={styles.containerInfos}>
+                      <div className={styles.title}>
+                        <p>{event.name}</p>
+                        <p>Local: {event.empresa.name}</p>
+                      </div>
+
+                      <div className={styles.infos}>
+                        <p>{event.type} / {event.category}</p>
+                        <p>R$ {event.price}</p>
+                      </div>
                     </div>
 
-                    <div className={styles.infos}>
-                      <p>{event.type} / {event.category}</p>
-                      <p>R$ {event.price}</p>
+                    <div>
+                      <InfoHour type="greenHour" >{handleDate(event.startAt)} </InfoHour>
                     </div>
                   </div>
-
-                  <div>
-                    <InfoHour type="greenHour" >{handleDate(event.startAt)} </InfoHour>
-                  </div>
-                </div>
-            </div>
+              </div>
+            </>
+            
           )
         })
       }
-            
-
-            <LineDividing/>
-
-            {/* TESTE INFOHOUR AMARELO */}
-            <div className={styles.containerEvento}>
-                <p>Rua Copacabana, 1234 - Copacabanda - 56743-876</p>
-                
-                <div className={styles.containerDetails}>
-                  <div className={styles.containerInfos}>
-                    <div className={styles.title}>
-                      <p>Morrendo de Rir</p>
-                      <p>Local: Circo Voador</p>
-                    </div>
-
-                    <div className={styles.infos}>
-                      <p>Standup / Comedy</p>
-                      <p>R$ 15,00</p>
-                    </div>
-                  </div>
-
-                  <div>
-                    <InfoHour type="yellowHour">11:20</InfoHour>
-                  </div>
-                </div>
-            </div>
 
             <LineDividing/>
 
