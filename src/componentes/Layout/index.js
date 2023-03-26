22809a9b2ac85f34c1eef5b32b45f04f43cc40c9
Mjfreedom <mbuzina@gmail.com>
2023-03-25 21:38:52 -0300

    :lipstick: feat: Estilizaçao CSS arrumando detalhes do map page

--- src/componentes/Layout/index.js
+++ src/componentes/Layout/index.js
@@ -3,9 +3,9 @@ import { Navbar } from '../Navbar'
 
 export const Layout = (props) => {
   return (
-    <>
+    <div className='layout-container'>
         <Navbar/>
         {props.children}
-    </>
+    </div>
   )
 }
