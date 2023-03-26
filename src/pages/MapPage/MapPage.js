2811a4ba3f84674715b21d0a67ea1670b2076670
Mjfreedom <mbuzina@gmail.com>
2023-03-26 14:31:56 -0300

    :lipstick: feat: Estilizaçao CSS das tags do map finalizado

--- src/pages/MapPage/MapPage.js
+++ src/pages/MapPage/MapPage.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import "./styled.css"
-import mapImage from '../../assets/rioMap.jpeg'
+import mapImage from '../../assets/mapTag.png'
 import { Layout } from '../../componentes/Layout'
 import { MapCard } from '../../componentes/MapCard/MapCard'
+import { HiLocationMarker } from 'react-icons/hi'
+import { BiCurrentLocation } from 'react-icons/bi'
 
 export const MapPage = () => {
     return (
@@ -10,12 +12,27 @@ export const MapPage = () => {
             <div className='coitainer-map'>
                 <div className='container-img'>
                     <img className='map-img' src={mapImage} alt='Map Image' />
-                    <div className='group-zoom'>
+                    <div id='tag-map1'>
+                        <HiLocationMarker />
+                    </div>
+                    <div id='tag-map2'>
+                        <HiLocationMarker />
+                    </div>
+                    <div id='tag-map3'>
+                        <HiLocationMarker />
+                    </div>
+                    <div id='tag-map4'>
+                        <HiLocationMarker />
+                    </div>
+                    <div id='tag-map5'> 
+                        <BiCurrentLocation />
+                    </div>
+                    <div id='group-zoom'>
                         <div className='zoom-container'>
-                        <button className='zoom-btn'>+</button>
-                        <button className='zoom-btn'>-</button>
+                            <button className='zoom-btn'>+</button>
+                            <button className='zoom-btn'>-</button>
                         </div>
-                        
+
                     </div>
                 </div>
                 <MapCard />
